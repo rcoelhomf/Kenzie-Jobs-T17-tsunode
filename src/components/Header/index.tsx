e51e8e2a23a46d7a3b370297a801d1c55561f180
OseasSC <oseas.stoterau1@hotmail.com>
2023-07-03 15:23:38 -0300

    Header simples added

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import jobs from "../../assets/jobs.png";
+import magnifier from "../../assets/lupa.png";
+import { StyledDiv, StyledImg } from './style';
+
+export const Header = () => {
+    return (
+        <StyledDiv>
+            <StyledImg src={jobs} alt="imagem da escrita jobs" width="69px" height="30px" />
+            <div>
+                <Link to={"/"}>acesso empresa</Link>
+                <button>Confira nossas vagas</button>
+                <StyledImg src={magnifier} alt="lupa de procurar" width="30px" height="30px" />
+            </div>
+        </StyledDiv>
+    );
+};
