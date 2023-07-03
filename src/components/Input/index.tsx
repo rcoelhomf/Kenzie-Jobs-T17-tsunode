3d26537e6094d25f875cdc8698cfb536fcc68bb2
rcoelhomf <rcoelhomf@hotmail.com>
2023-07-03 15:14:10 -0300

    feat: botando titulo do input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,17 +3,19 @@ import { ForwardedRef, InputHTMLAttributes, forwardRef } from "react"
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement>{
 
     error: any;
+    label: string
 }
 
 // Trocar tipagem do erro depois
 
 export const Input = forwardRef(
-    ({error, ...rest}: IInputProps, ref: ForwardedRef<HTMLInputElement>) => {
+    ({error,label, type, ...rest}: IInputProps, ref: ForwardedRef<HTMLInputElement>) => {
 
     return (
 
         <div>
-            <input ref={ref} type="text" {...rest} />
+            {label ? <p>{label}</p> : null}
+            <input ref={ref} type={type ? type : "text"} {...rest} />
             {error ? <p>{error.message}</p> : null}
         </div>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { App } from "./App"
 import { BrowserRouter } from "react-router-dom"
-import { CompanyProvider } from "./providers/CompanyContext.tsx"
+import { CompanyProvider } from "./providers/CompanyContext/index.tsx"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
